1dd1c859d91d51c41dbf627245c55d9767f8edde
KodeUniverse <dakshin@gmail.com>
2025-10-07 01:16:46 -0400

    Added auto-complete for JS/TS.

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,73 @@
+return {
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+    },
+    config = function()
+      local cmp = require('cmp')
+      
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lsp_signature_help' },
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+    end,
+  },
+  
+  -- LSP Configuration
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      
+      -- Example: setup TypeScript/JavaScript LSP
+      vim.lsp.config['ts_ls'] = {
+        cmd = { 'typescript-language-server', '--stdio' },
+        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json' },
+        capabilities = capabilities,
+      }
+      vim.lsp.enable('ts_ls')
+      
+      -- Add more language servers as needed
+    end,
+  },
+
+  -- Prettier
+  {
+    'prettier/vim-prettier',
+    build = 'yarn install --frozen-lockfile --production',
+    ft = {
+      'javascript',
+      'typescript',
+      'css',
+      'scss',
+      'json',
+      'graphql',
+      'markdown',
+      'vue',
+      'yaml',
+      'html'
+    },
+  },
+}
