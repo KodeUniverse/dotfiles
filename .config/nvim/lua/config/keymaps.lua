ffa5ce231b60a3e86ba7a43625a19c7fde0c421a
KodeUniverse <kodeuniverse@proton.me>
2026-03-11 09:43:53 -0400

    added fzf-lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>f", ":Lf<CR>", { desc = "Open lf" })
 
 -- Theme Picker
 vim.keymap.set("n", "<leader>t", ":Themery<CR>", { desc = "Open Themery" })
+
+-- Fzf-lua
+vim.keymap.set("n", "<leader>z", ":Fzf files<CR>", {desc = "Open fzf-lua"})
